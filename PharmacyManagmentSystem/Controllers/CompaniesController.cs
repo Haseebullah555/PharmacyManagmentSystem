0c07410128e2e65fd08c9ddea62455bb85344c1a
Riasat Nabi <nabiafghan1516@gmail.com>
2024-03-06 01:49:44 -0800

    add company controller as well as the index and create methodes

---
+++ PharmacyManagmentSystem/Controllers/CompaniesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PharmacyManagmentSystem.Data;
+using PharmacyManagmentSystem.Models;
+
+namespace PharmacyManagmentSystem.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public CompaniesController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+
+            return View(db.Companies.ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+       public IActionResult Create(Company cm)
+        {
+
+            if(ModelState.IsValid)
+            {
+                db.Companies.Add(cm);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+    }
+}
