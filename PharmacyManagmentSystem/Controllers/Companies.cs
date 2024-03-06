055eee510ce55b6db209b849b538a9b1f1e19727
haseeb555 <hasibullahshirzadahsf@gmail.com>
2024-03-06 12:59:07 +0430

    add company Controller

---
+++ PharmacyManagmentSystem/Controllers/Companies.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PharmacyManagmentSystem.Controllers
+{
+    public class Companies : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
