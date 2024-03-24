d5f91bb5da45aabe710bbae3abee917e9aea9412
haseeb555 <hasibullahshirzadahsf@gmail.com>
2024-03-24 10:05:34 +0430

    make some changes into sale controller

--- PharmacyManagmentSystem/Controllers/SalesController.cs
+++ PharmacyManagmentSystem/Controllers/SalesController.cs
@@ -15,7 +15,7 @@ namespace PharmacyManagmentSystem.Controllers
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
 
-        public SalesController(ApplicationDbContext context,IMapper mapper)
+        public SalesController(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
@@ -23,7 +23,7 @@ namespace PharmacyManagmentSystem.Controllers
         public IActionResult Index()
         {
 
-            var query = _context.Sales.Include(m=>m.Medicine).Include(c=>c.Customer).Include(cr=>cr.Currency).ToList();
+            var query = _context.Sales.Include(m => m.Medicine).Include(c => c.Customer).Include(cr => cr.Currency).ToList();
             return View(query);
         }
         public IActionResult Create()
@@ -52,18 +52,8 @@ namespace PharmacyManagmentSystem.Controllers
                 Text = $"{item.TradeName} | {item.Capacity} | {item.CompanyName} | {item.CurrencyName}",
                 Value = $"{item.TradeName}_{item.Capacity}_{item.CompanyName}_{item.CurrencyName}_{item.Amount}_{item.SalePrice}"
             })
-            .ToList();
-            var selectedMedicine = query.FirstOrDefault();
-            var amount = selectedMedicine?.Amount ?? 0;
+                .ToList();
             ViewBag.medicines = selectableMedicines;
-            ViewBag.Amount = amount;
-
-            //var defaultItem = new SelectListItem()
-            //{
-            //   Text = "--Select a Medicine",
-            //    Value = ""
-            //};
-            //query.Insert(0, defaultItem);
 
             var currency = _context.Currencies
                 .Select(c => new SelectListItem()
@@ -82,49 +72,28 @@ namespace PharmacyManagmentSystem.Controllers
                 })
                 .ToList();
             ViewBag.customer = customer;
-
-            //var amount = _context.Purchases
-            //    .GroupBy(p => new { p.Medicine.TradeName, p.Medicine.Capacity, p.Medicine.Company.CompanyName, p.CurrencyID })
-            //    .Select(g => new
-            //    {
-            //        Amount = g.Sum(p => p.Amount)
-            //    })
-            //    .ToList();
-
-            //ViewBag.Amount = amount.Select(a => new SelectListItem
-            //{
-            //    Text = a.Amount.ToString(),
-            //    Value = a.Amount.ToString()
-            //}).ToList();
-
-            //var salePriceQuery = _context.Purchases
-            //    .GroupBy(p => new { p.Medicine.TradeName, p.Medicine.Capacity, p.Medicine.Company.CompanyName, p.CurrencyID })
-            //    .Select(g => new
-            //    {
-            //        SalePrice = g.FirstOrDefault(p => p.PurchaseDate == g.Max(d => d.PurchaseDate)).SalePrice
-            //    })
-            //    .ToList();
-            //ViewBag.SalePrice = salePriceQuery.Select(s => s.SalePrice).ToList();
-            //var query = _context.Purchases
-            //    .GroupBy(p => new { p.Medicine.TradeName, p.Medicine.Capacity, p.Medicine.Company.CompanyName, p.CurrencyID })
-            //    .Select(g => new
-            //    {
-            //        Amount = g.Sum(p => p.Amount),
-            //        SalePrice = g.FirstOrDefault(p => p.PurchaseDate == g.Max(d => d.PurchaseDate)).SalePrice
-            //    })
-            //    .ToList();
-            //var amountList = query.Select(q => q.Amount).ToList();
-            //var salePriceList = query.Select(q => q.SalePrice).ToList();
-           
             return View();
         }
         [HttpPost]
-        [HttpPost]
         public IActionResult Create(SaleViewModel viewModel)
         {
-            Sale sale = _mapper.Map<Sale>(viewModel);
             if (ModelState.IsValid)
             {
+                //Sale sale = _mapper.Map<Sale>(viewModel);
+
+               Sale sale = new Sale
+               {
+                  SaleAmount = viewModel.SaleAmount,
+                  SaleDate = viewModel.SaleDate,
+                  SaleID = viewModel.SaleID,
+                  Paid  = viewModel.Paid,
+                  UnitPrice = viewModel.UnitPrice,
+                  Unpaid    = viewModel.Unpaid,
+                  MedicineID = viewModel.MedicineID,
+                  CurrencyID = viewModel.CurrencyID,
+                  CustomerID    = viewModel.CustomerID,
+                  TotalPrice = viewModel.TotalPrice,
+               };
                 _context.Sales.Add(sale);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
@@ -146,18 +115,5 @@ namespace PharmacyManagmentSystem.Controllers
             }
             return View(purchase);
         }
-        //[HttpPost]
-        //[HttpPost]
-        //public IActionResult GetAmount(string medicineID)
-        //{
-        //    string pattern = @"\d+$";
-        //    Match match1 = Regex.Match(medicineID, pattern);
-        //    if (match1.Success)
-        //    {
-        //        string extractedValue1 = match1.Value;
-        //        Console.WriteLine(extractedValue1);
-        //    }
-        //    return Json(match1);
-        //}
     }
 }
